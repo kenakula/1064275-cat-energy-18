70cd4d0fa3b960ae546ef06c5083ec9e1eddfc65
Kenan Agaev <kenkosh@yandex.ru>
2019-12-19 21:40:06 +0300

    Выполняет слайдер с учетом фонов

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,13 +1,8 @@
 var burgerButton = document.querySelector('.main-nav__toggle');
-var nav = document.querySelector('.main-nav');
-var menu = document.querySelector('.main-nav__list');
 
-nav.classList.add('main-nav--closed');
-menu.classList.add('hidden');
+burgerButton.classList.add('main-nav__toggle--open');
 
 burgerButton.addEventListener('click', function() {
-  menu.classList.toggle('hidden')
-
-  nav.classList.toggle('main-nav--closed')
-  nav.classList.toggle('main-nav--opened')
+  burgerButton.classList.toggle('main-nav__toggle--open');
+  burgerButton.classList.toggle('main-nav__toggle--close');
 })
--- source/js/slider.js
+++ source/js/slider.js
@@ -6,26 +6,62 @@ var afterBtn = document.querySelector('.slider__button--after');
 
 var rangeBtn = document.querySelector('.range__button');
 
-afterBtn.addEventListener('click', function() {
-  if (slimCat.classList.contains('slider__item--hidden')) {
-    slimCat.classList.toggle('slider__item--hidden');
-    fatCat.classList.toggle('slider__item--hidden');
-    rangeBtn.classList.add('range__button--after');
+var slider = document.querySelector('.slider');
+var section = document.querySelector('.example');
 
-    if (rangeBtn.classList.contains('range__button--before')) {
-      rangeBtn.classList.remove('range__button--before');
+var stats = document.querySelectorAll('.example__stats');
+
+window.addEventListener('resize', function() {
+  if (window.matchMedia("(min-width: 1440px)").matches) {
+    if (!slimCat.classList.contains('slider__item--hidden')) {
+      //изменяет фон при увеличении размера окна и активном худом котике
+      section.classList.add('example--slim')
+      //изменяет фон елементам списка определений
+      for (i = 0; i < stats.length; i++) {
+        stats[i].classList.add('example__stats--slim')
+      }
+    }
+  }
+})
+
+afterBtn.addEventListener('click', function() {
+  //изменяет фото на худого котика
+  slimCat.classList.remove('slider__item--hidden');
+  fatCat.classList.add('slider__item--hidden');
+  //двигает ползунок в мобильной версии
+  rangeBtn.classList.add('range__button--after');
+
+  //изменяет фон на планшетах
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    slider.classList.add('example__slider--slim')
+  }
+  //изменяет фон на десктопах
+  if (window.matchMedia("(min-width: 1440px)").matches) {
+    section.classList.add('example--slim')
+    //изменяет фон елементам списка определений
+    for (i = 0; i < stats.length; i++) {
+      stats[i].classList.add('example__stats--slim')
     }
   }
 })
 
 beforeBtn.addEventListener('click', function() {
-  if (!slimCat.classList.contains('slider__item--hidden')) {
-    slimCat.classList.toggle('slider__item--hidden');
-    fatCat.classList.toggle('slider__item--hidden');
-    rangeBtn.classList.add('range__button--before');
-
-    if (rangeBtn.classList.contains('range__button--after')) {
-      rangeBtn.classList.remove('range__button--after');
+  //изменяет фото на толстого котика
+  fatCat.classList.remove('slider__item--hidden');
+  slimCat.classList.add('slider__item--hidden');
+  //двигает ползунок в мобильной версии
+  rangeBtn.classList.remove('range__button--after');
+  //изменяет фон на планшетах
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    slider.classList.remove('example__slider--slim')
+  }
+  //изменяет фон на десктопах
+  if (window.matchMedia("(min-width: 1440px)").matches) {
+    slider.classList.remove('example__slider--slim')
+    section.classList.remove('example--slim')
+    //изменяет фон елементам списка определений
+    for (i = 0; i < stats.length; i++) {
+      stats[i].classList.remove('example__stats--slim')
     }
   }
 })
