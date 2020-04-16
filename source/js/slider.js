7ff4de1fa92a0e144e6ce6f0f5781b6fe24ddcff
Kenan Agaev <kenkosh@yandex.ru>
2020-04-16 19:08:36 +0300

    Реализует переключение слайдов на JQuery

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,64 +1,25 @@
-'use strict';
+$(document).ready(function () {
 
-(function () {
+  const slimColor = "#eaeaea";
 
-  var slider = document.querySelector('.slider');
-  var sliderList = slider.querySelector('.slider__items-list');
-  var sliderItems = sliderList.querySelectorAll('.slider__item');
+  const showSlimSlide = function () {
+    $(".slider__item").eq(0).fadeOut();
+    $(".slider__item").eq(1).fadeIn();
+    $(".range__button").addClass("range__button--right");
 
-  var controlsContainer = slider.querySelector('.slider__controls');
-  var rangeButton = controlsContainer.querySelector('.range__button');
-
-  var newSlideFadeOut = function (slide) {
-    var opacity = 0;
-    var timeout = setTimeout(function tick () {
-      if (opacity < 11) {
-        slide.style.opacity = opacity / 10;
-        opacity++;
-        timeout = setTimeout(tick, 50);
-      }
-    }, 50)
-  };
-
-  var activeSlideFadeIn = function (slide) {
-    var opacity = 1;
-    var timeout = setTimeout(function tick () {
-      if (opacity < 11) {
-        slide.style.opacity = 1 - opacity / 10;
-        opacity++;
-        timeout = setTimeout(tick, 50);
-      }
-    }, 50);
-  };
-
-  var changeSlideOpacity = function (activeSlide, newSlide) {
-    newSlideFadeOut(newSlide);
-    activeSlideFadeIn(activeSlide);
-  };
-
-  var changeRangeButtonPosition = function (activeSlide) {
-    switch (activeSlide) {
-      case '1':
-        rangeButton.classList.remove('range__button--left');
-        rangeButton.classList.add('range__button--right');
-        break;
-      default:
-        rangeButton.classList.remove('range__button--right');
-        rangeButton.classList.add('range__button--left');
+    if ($(window).width() >= "1300") {
+      $(".example").css("background-color", slimColor);
     }
   };
 
-  var changeSlide = function (evt) {
-    var currentSlide = sliderList.querySelector('.slider__item--active')
-
-    if (evt.target.classList.contains('button')) {
-      var newSlideIndex = evt.target.dataset.index;
-      currentSlide.classList.toggle('slider__item--active');
-      sliderItems[newSlideIndex].classList.toggle('slider__item--active');
-      changeRangeButtonPosition(newSlideIndex);
-      changeSlideOpacity(currentSlide, sliderItems[newSlideIndex]);
-    }
+  const showFatSlide = function () {
+    $(".slider__item").eq(0).fadeIn();
+    $(".slider__item").eq(1).fadeOut();
+    $(".example").css("background-color", "");
+    $(".range__button").removeClass("range__button--right");
   };
 
-  controlsContainer.addEventListener('click', changeSlide);
-})();
+  $(".slider__button--after").click(showSlimSlide);
+  $(".slider__button--before").click(showFatSlide);
+
+});
